b20d2e48f29aa283cd121f2095b25c998613fd33
Giovanni Colasante <giovanni.colasante@gmail.com>
2018-03-30 23:35:45 +0200

    Added a comment in reference to README.md

--- postdata.php
+++ postdata.php
@@ -6,16 +6,16 @@ if (!isset($_POST) || empty($_POST)) {
 include __DIR__.'/config/reCaptchaV2.php';
 include __DIR__.'/lib/checkCaptchaV2.php';
 
+// First step of validation
 $check = checkGoogleCaptchaV2($endpoint, [
         // These parameters can not be altered by users.
         'secret'   => $secretKey,
         'remoteip' => $_SERVER['REMOTE_ADDR'],
-        // At moment there no info how to validate this response.
+        // At moment there is no info how to validate this response.
         'response' => $_POST['g-recaptcha-response'],
     ]);
     
 if ($check === FALSE) {
-    echo "FALSE";
     return FALSE;
 }
 
