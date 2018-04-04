51eb22557c33926b692361e3f6cd4630e8a09acd
Giovanni Colasante <giovanni.colasante@gmail.com>
2018-04-04 21:14:19 +0200

    Changed how parameters are handled.

--- config/reCaptchaV2.php
+++ config/reCaptchaV2.php
@@ -1,4 +1,6 @@
 <?php
-$htmlKey   = 'Your HTML key';
-$secretKey = 'Your secret Key';
-$endpoint  = 'https://www.google.com/recaptcha/api/siteverify';
+return array(
+    'htmlKey'   => 'Your HTML key',
+    'secretKey' => 'Your secret Key',
+    'endpoint'  => 'https://www.google.com/recaptcha/api/siteverify',
+);
--- postdata.php
+++ postdata.php
@@ -2,17 +2,17 @@
 if (!isset($_POST) || empty($_POST)) {
     return FALSE;
 }
-
-include __DIR__.'/config/reCaptchaV2.php';
 include __DIR__.'/lib/checkCaptchaV2.php';
 
+$params = include __DIR__.'/config/reCaptchaV2.php';
+
 // First step of validation
-$check = checkGoogleCaptchaV2($endpoint, [
+$check = checkGoogleCaptchaV2($params['endpoint'], [
         // These parameters can not be altered by users.
-        'secret'   => $secretKey,
-        'remoteip' => $_SERVER['REMOTE_ADDR'],
+        'secret'      => $params['secretKey'],
+        'remoteip'    => $_SERVER['REMOTE_ADDR'],
         // At moment there is no info how to validate this response.
-        'response' => $_POST['g-recaptcha-response'],
+        'g-recaptcha' => $_POST['g-recaptcha-response'],
     ]);
     
 if ($check === FALSE) {
